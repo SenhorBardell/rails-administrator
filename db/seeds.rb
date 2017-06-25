7281a12f5299f50286314150b3149361923deb8f
John <senhorbardell@gmail.com>
2017-06-25 17:59:32 +0300

    add

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,5 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Role.create(name: 'admin', id: 1)
+Role.create(name: 'client', id: 2)
