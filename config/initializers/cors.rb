546e1a091e16b10d790d0467a925128d9f83f996
John <senhorbardell@gmail.com>
2017-06-26 12:37:50 +0300

    allow cors

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'github.com/SenhorBardell/react-administrator', 'localhost:3000'
+    origins '*'
 
     resource '*',
              headers: :any,
